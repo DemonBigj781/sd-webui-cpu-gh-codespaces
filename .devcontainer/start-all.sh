fe5d891a5d269744058020003a83a2f66578ed61
DemonBigj781 <20097331+DemonBigj781@users.noreply.github.com>
2025-11-13 17:01:26 -0500

    Remove JupyterLab startup from start-all.sh
    Removed JupyterLab startup from the script to streamline the container initialization process.

--- .devcontainer/start-all.sh
+++ .devcontainer/start-all.sh
@@ -9,23 +9,11 @@ mkdir -p /var/run/sshd
 cd /app
 nohup ./horde-scribe-bridge.sh > /var/log/horde-scribe.log 2>&1 &
 
-# Start JupyterLab on port 8888, no token
-# Use python3 -m jupyterlab so we don't care what the CLI binary is called
-cd /workspaces || cd /app
-nohup python3 -m jupyterlab \
-  --ip=0.0.0.0 \
-  --port=8888 \
-  --no-browser \
-  --ServerApp.token='' \
-  --ServerApp.password='' \
-  --ServerApp.allow_origin='*' \
-  > /var/log/jupyter.log 2>&1 &
-
 # Start simple FTP server on port 2121
 nohup python3 -m pyftpdlib -p 2121 > /var/log/ftp.log 2>&1 &
 
 # Make sure logs exist so tail doesn't crash
-touch /var/log/horde-scribe.log /var/log/jupyter.log /var/log/ftp.log
+touch /var/log/horde-scribe.log /var/log/ftp.log
 
 # Keep container alive by tailing logs
-tail -F /var/log/horde-scribe.log /var/log/jupyter.log /var/log/ftp.log
+tail -F /var/log/horde-scribe.log /var/log/ftp.log
